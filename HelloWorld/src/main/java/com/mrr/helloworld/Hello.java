da843fe9db492ed8fd48dedaf43f163cda40531b
Melissa Rogenski <rogenskim@gmail.com>
2021-10-25 15:48:22 -0500

    Adding my first hello world program in an IDE to my classwork repo. - Melissa Rogenski

---
+++ HelloWorld/src/main/java/com/mrr/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author Melissa Rogenski
+ * email: rogenskim@gmail.com
+ * date: 10/25/21
+ * purpose: My first Hello World program in an IDE
+ */
+
+package com.mrr.helloworld;
+
+public class Hello {
+    public static void main (String[] args) {
+	System.out.println("Hello, World!");
+    }
+}
